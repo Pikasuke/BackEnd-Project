7ebe21200beeba451a537087ba71d8df4619464d
pikasuke <tongfongctm@hotmail.com>
2022-05-18 21:35:27 +0200

    MAJ des entités

---
+++ eBankBackEnd/src/main/java/com/example/eBankBackEnd/EBankBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.eBankBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBankBackEndApplication.class, args);
+	}
+
+}
---
+++ eBankBackEnd/src/main/java/com/example/eBankBackEnd/entities/Customer.java
@@ -0,0 +1,5 @@
+package com.example.eBankBackEnd.entities;
+
+public class Customer {
+
+}
